54a0df60a7ff3bbbada594796afd5e8381d026c6
Mangiang <mangiang@orange.fr>
2022-01-13 12:11:56 -0500

    feat: Add liveness probe

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 python -m pip install -r requirements.txt
 # Uncomment for verbose mode
-python -m kopf run app.py --verbose
+python -m kopf run app.py --verbose --liveness=http://0.0.0.0:8080/healthz
 # python -m kopf run app.py
